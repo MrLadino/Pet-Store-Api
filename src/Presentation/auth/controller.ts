d0e89318599b9f26d7c684afb7888610da906ded
Andrés <andresladino0324@gmail.com>
2024-06-19 14:54:44 -0500

    Implementacion Manejo de errores en el controlador

--- src/Presentation/auth/controller.ts
+++ src/Presentation/auth/controller.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from 'express'; // 5
-import { RegisterUserDto, AuthRepository } from './../../domain'; // 1
-
-
-
+import { AuthRepository, CustomError,  RegisterUserDto } from '../../domain';
 
 export class AuthController { // 1
 
@@ -10,14 +7,24 @@ export class AuthController { // 1
         private readonly authRepository: AuthRepository //9
     ){} // 2
 
-    registerUser = (req: Request, res: Response) => { // 4
-        const [error, registerUserDto] = RegisterUserDto.create(req.body);
-        if ( error ) return res.status(400).json({ error });
+    private handleError = ( error: unknown, res: Response ) => {
+    
+         if ( error instanceof CustomError ) {
+           return res.status(error.statusCode).json({ error: error.message });
+         }
+         return res.status(500).json({ error: 'Internal Server Error' });
+       }
 
-        this.authRepository.register(registerUserDto!) //10
-        .then( user => res.json(user) ) // 11
-        .catch( error => res.status(500).json(error) ) // 12
-    } // 3
+    registerUser = (req: Request, res: Response) => {
+        // 4
+        const [error, registerUserDto] = RegisterUserDto.create(req.body);
+        if (error) return res.status(400).json({ error });
+    
+        this.authRepository
+          .register(registerUserDto!) //10
+          .then((user) => res.json(user)) // 11
+          .catch((error) => this.handleError(error, res)); // 12
+      }; // 3
 
     loginUser =  (req: Request, res: Response) => { // 7
         res.json('loginUser controller') // 8
